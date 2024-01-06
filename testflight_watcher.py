7ac36c76e1a7db03e00a8378181a48191e1d527d
Samuel Gräfenstein <samgraf00@gmail.com>
2024-01-07 00:07:10 +0100

    Fix sending message for already open betas
    This fixes the script not sending a message when a beta is already
    open on startup.

--- testflight_watcher.py
+++ testflight_watcher.py
@@ -24,14 +24,12 @@ def watch(watch_ids, callback, notify_full=True, sleep_time=900):
                 headers={"Accept-Language": "en-us"})
             page = html.fromstring(req.text)
             free_slots = page.xpath(XPATH_STATUS)[0] not in FULL_TEXTS
-            if tf_id not in data:
+
+            if (tf_id not in data or data[tf_id] != free_slots):
+                if free_slots or notify_full:
+                    title = re.findall(
+                        TITLE_REGEX,
+                        page.xpath(XPATH_TITLE)[0])[0]
+                    callback(tf_id, free_slots, title)
                 data[tf_id] = free_slots
-            else:
-                if data[tf_id] != free_slots:
-                    if free_slots or notify_full:
-                        title = re.findall(
-                            TITLE_REGEX,
-                            page.xpath(XPATH_TITLE)[0])[0]
-                        callback(tf_id, free_slots, title)
-                    data[tf_id] = free_slots
         sleep(sleep_time)
